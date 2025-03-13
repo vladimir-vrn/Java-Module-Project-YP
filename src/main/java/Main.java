03117107c449f890882525d389ba0786a523e1c4
vladimir-vrn <dvv_mail@mail.ru>
2025-03-13 19:45:43 +0300

    Проектная работа №1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,44 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Race race = new Race();
+        byte numberCar = 1;
+        Car car = new Car("", 0);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            if (car.name.isEmpty()) {
+                System.out.println("— Введите название машины №" + numberCar + ": ");
+                car.name = scanner.nextLine().trim();
+                if (car.name.isEmpty()) {
+                    System.out.println("— Неправильное название машины");
+                    continue;
+                }
+            }
+
+            System.out.println("— Введите скорость машины №" + numberCar + ": ");
+            if (!scanner.hasNextInt()) {
+                System.out.println("— Неправильная скорость");
+                scanner.next();
+                continue;
+            }
+            car.speed = scanner.nextInt();
+            scanner.nextLine();
+            if (car.speed < 0 || car.speed > 250) {
+                System.out.println("— Неправильная скорость");
+                continue;
+            }
+            race.findLeader(car);
+
+            if (numberCar == 3) break;
+            car = new Car("", 0);
+            numberCar++;
+        }
+
+        System.out.println("Самая быстрая машина: " + race.leader);
+        scanner.close();
     }
 }
